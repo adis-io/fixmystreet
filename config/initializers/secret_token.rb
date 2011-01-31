1060805c454bce0be325642cd0b5ef4f27ca5f4b
Adis Osmonov <adis.osmonov@gmail.com>
2011-01-31 21:46:48 +0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fixmystreet::Application.config.secret_token = 'fe5392a3ad142157229660c9170213b2d90f3319b9db1027f5dbe21b109724e2c984ad9d27369f80e44ae3ea3afeddbdcc17dcbd312417e69f196b0d4e53b13c'
