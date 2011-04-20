34d89343cc371e6231029d7d95e84d7b08756da1
Kalys Osmonov <kalys@osmonov.com>
2011-04-20 18:12:18 +0600

    Twitter geo data

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -92,7 +92,7 @@ class ReportsController < ApplicationController
       client = Twitter::Client.new
       text = "#{report_url(report)} #{report.title}"
       text = text[0...140]
-      client.update(text)
+      client.update text, :lat => report.latitude, :long => report.longtitude
     end
   rescue
   end
