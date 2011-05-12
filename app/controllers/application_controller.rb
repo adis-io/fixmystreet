ae82e109d975f33f0ac99d47b2ee8342fea11708
Kalys Osmonov <kalys@osmonov.com>
2011-05-12 23:25:33 +0600

    Locale fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     session[:locale] = params[:locale] if params[:locale] and %(ru en).include? params[:locale]
-    I18n.locale = session[:locale] if session[:locale].nil?
+    I18n.locale = session[:locale] unless session[:locale].nil?
   end
 
   def fetch_city
