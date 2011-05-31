8d07b4102c132b44bfe535d6dbfa619f31e3fc3d
Kalys Osmonov <kalys@osmonov.com>
2011-05-31 23:26:41 +0600

    Subdomain configuration; City lat;lng

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,17 @@
 # Be sure to restart your server when you modify this file.
-
-Fixmystreet::Application.config.session_store :cookie_store,
-  :key => '_fixmystreet_session',
-  :domain => '.fixmystreet.kg'
+if Rails.env.production?
+  Fixmystreet::Application.config.session_store :cookie_store,
+    :key => '_fixmystreet_session',
+    :domain => '.fixmystreet.kg'
+elsif Rails.env.development?
+  Fixmystreet::Application.config.session_store :cookie_store,
+    :key => '_fixmystreet_session',
+    :domain => :all
+elsif Rails.env.test?
+  Fixmystreet::Application.config.session_store :cookie_store,
+    :key => '_fixmystreet_session',
+    :domain => :all
+end
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
--- config/initializers/subdomainfu.rb
+++ config/initializers/subdomainfu.rb
@@ -1,6 +1,6 @@
 SubdomainFu.configure do |config|
   config.tld_sizes = {
-    :development => 1,
+    :development => 2,
     :production => 1,
     :test => 1 }
 end
