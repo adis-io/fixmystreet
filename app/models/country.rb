4ae2e78a9b694b962a30319467c5cbb6d29f0187
Kalys Osmonov <kalys@osmonov.com>
2011-09-25 21:57:40 +0600

    Fix country model

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,6 @@
 class Country < ActiveRecord::Base
-  attr_accessible :name, :lat, :long, :zoom, :country_code
-  validates_presence_of :name, :lat, :long, :zoom, :country_code
+  attr_accessible :name, :lat, :lng, :zoom, :country_code
+  validates_presence_of :name, :lat, :lng, :zoom, :country_code
   validates_uniqueness_of :name, :country_code
   has_many :cities
   has_one :default_city, :class_name => "City"
