acbc4a0ab2ec860378954ed0b5ba88aa059a9021
Kalys Osmonov <kalys@osmonov.com>
2011-12-23 14:11:41 +0600

    Twitter consumer secret is fixed

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   SOCIAL_NETWORK_CONFIG = {
     "twitter"=> {
       "consumer_key" => ENV["TWITTER_CONSUMER_KEY"],
-      "consumer_secret" => ENV["TWITTER_CONSUMER_KEY"],
+      "consumer_secret" => ENV["TWITTER_CONSUMER_SECRET"],
       "oauth_token" => ENV["TWITTER_OAUTH_TOKEN"],
       "oauth_token_secret" => ENV["TWITTER_OAUTH_TOKEN_SECRET"]
     },
